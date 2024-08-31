0fbd5da6eeadca5327982efa5949495b099b8a61
xukaiyyds <3353212714@qq.com>
2024-09-01 01:37:01 +0800

    first deploy

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">每一个不曾起舞的日子，都是对生命的辜负。</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
