d95dc06d68840a38d2081537f4327a5435dcd754
xukaiyyds <3353212714@qq.com>
2025-03-15 22:38:01 +0800

    update

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, Image, Code, Book, CompactDisc, LaptopCode, Cloud, Eye, Dragon } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Image, Code, Book, CompactDisc, Dragon, Eye, Cloud, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -68,10 +68,10 @@ const siteIcon = {
   Code,
   Book,
   CompactDisc,
-  LaptopCode,
-  Cloud,
-  Eye,
   Dragon,
+  Eye,
+  Cloud,
+  LaptopCode,
 };
 
 // 链接跳转
