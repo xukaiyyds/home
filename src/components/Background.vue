63b2c5a080218b659a68467cef66be99f0ddf52d
xukaiyyds <3353212714@qq.com>
2026-08-16 20:13:16 +0800

    修复每日一图显示错误问题

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -41,7 +41,7 @@ const changeBg = (type) => {
   if (type == 0) {
     bgUrl.value = `/images/background${bgRandom}.jpg`;
   } else if (type == 1) {
-    bgUrl.value = "https://api.dujin.org/bing/1920.php";
+    bgUrl.value = "https://api.xinyew.cn/api/bing";
   } else if (type == 2) {
     bgUrl.value = "https://tu.ltyuanfang.cn/api/fengjing.php";
   } else if (type == 3) {
